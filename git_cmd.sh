86eb223326fb4b433204436e221e5057bb20245a
kazushi <kimoto@okayama-u.ac.jp>
2019-10-04 09:41:15 +0900

    Open Git Repository for Struct I Mech 2019

---
+++ git_cmd.sh
@@ -0,0 +1,11 @@
+git add A/Lec*/*.tex
+git add A/Lec*/*.svgz
+git add A/Lec*/makefile
+git add Memo*/*.tex
+git add Memo*/*.svgz
+git add Memo*/makefile
+
+git add git_cmd.sh
+
+git commit
+git push -u origin master
