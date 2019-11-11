654cb6bb37a6fd8f71cbe277d023024f3354c04d
kazushi <kimoto@okayama-u.ac.jp>
2019-11-11 20:44:13 +0900

    Files for IB imported

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,14 +1,17 @@
 git add A/Lec*/*.tex
 git add A/Lec*/*.svgz
 git add A/Lec*/makefile
+git add A/Exam*/*.tex
+git add A/Exam*/*.svgz
+git add A/Exam*/makefile
 
 git add Memo*/*.tex
 git add Memo*/*.svgz
 git add Memo*/makefile
 
-git add A/Exam*/*.tex
-git add A/Exam*/*.svgz
-git add A/Exam*/makefile
+git add B/Lec*/*.tex
+git add B/Lec*/*.svgz
+git add B/Lec*/makefile
 
 
 git add git_cmd.sh
