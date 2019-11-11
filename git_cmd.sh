6a45079e2832305a2b056b5c708e397b888d2f3a
kazushi <kimoto@okayama-u.ac.jp>
2019-11-11 19:32:24 +0900

    Exam updated

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,9 +1,15 @@
 git add A/Lec*/*.tex
 git add A/Lec*/*.svgz
 git add A/Lec*/makefile
+
 git add Memo*/*.tex
 git add Memo*/*.svgz
 git add Memo*/makefile
+
+git add A/Exam*/*.tex
+git add A/Exam*/*.svgz
+git add A/Exam*/makefile
+
 
 git add git_cmd.sh
 
